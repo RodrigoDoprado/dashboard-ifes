409a459694833aff738e0aa8906f4b30aa3f4b7b
Rodrigo do Prado <rodrigodoprado1992@gmail.com>
2023-09-19 23:44:56 -0300

    fix: page/login

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -1,6 +1,7 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import { AuthContext } from "../context/AuthContext";
+import "./Login.css"
 
 function Login (){
     const {signin} = useContext(AuthContext)
@@ -8,34 +9,41 @@ function Login (){
     const [password,setPassword]=useState("")
     const handleLogin = async (e: { preventDefault: () => void }) => {
         e.preventDefault();
-        const res = await signin(email, password)
-        if(res){window.location.href = window.location.href}else{alert("Email ou Senha Invalido")}
+        if(await signin(email, password)){window.location.href = window.location.href}else{alert("Email ou Senha Invalido")}
     }
+
+    useEffect(() => {
+        document.body.classList.add('login-body');
+        return () => {
+          document.body.classList.remove('login-body');
+        };
+      }, []);
+    
 
     return(
         <>
-        <Helmet><title>Login</title></Helmet>
-        <main className="main bg-primary gap-3 d-flex justify-content-center"style={{margin: "20vh 0 0 0"}}>
-            {/* <div className="container"> */}
-                {/* <div className="row"> */}
-                    <img className="d-none d-sm-block" src="./assets/ifes.png"/>
-                    <div className="card text-light bg-secondary bg-opacity-50" style={{width: "40vh"}}> {/*border-radius: "9px"*/}
-                        <div id="message"></div>
-                        <div className="card-body ">
-                            <form onSubmit={handleLogin} className="d-grid">
-                                <div className="card-head gap-3 d-inline-flex mb-4">
-                                    <img className="card-img-top d-block d-sm-none" src="./assets/ifes.png" style={{width:"45px", height:"45px"}}/>
-                                    <h1 className="card-title">Login</h1>
-                                </div>
-                                <div className="mb-3">
-                                    <input className="form-control p-3" type="email" name="email" id="email" placeholder="Email:" required value={email} onChange={event =>setEmail(event.target.value)}/>
-                                </div>
-                                <div className="mb-3">
-                                    <input className="form-control p-3" type="password" name="password" id="password" placeholder="Senha:" required value={password} onChange={event =>setPassword(event.target.value)}/>
-                                </div>
-                                <button className="btn btn-outline-secondary text-light px-3" type="submit">Avança</button>
-                            </form>
-                        </div>
+            <Helmet><title>Login</title></Helmet>
+            <main id="login" className="main gap-3 d-flex justify-content-center">
+                {/* <div className="container"> */}
+                    {/* <div className="row"> */}
+                        <img className="d-none d-sm-block" src="./assets/ifes.png"/>
+                        <div className="card text-light"> {/*border-radius: "9px"*/}
+                            <div id="message"></div>
+                            <div className="card-body ">
+                                <form onSubmit={handleLogin} className="d-grid">
+                                    <div className="card-head gap-3 d-inline-flex mb-4">
+                                        <img className="card-img-top d-block d-sm-none" src="./assets/ifes.png" style={{width:"45px", height:"45px"}}/>
+                                        <h1 className="card-title text-uppercase fw-bolder">Login</h1>
+                                    </div>
+                                    <div className="mb-3">
+                                        <input className="form-control p-3" type="email" name="email" id="email" placeholder="Email:" required value={email} onChange={event =>setEmail(event.target.value)}/>
+                                    </div>
+                                    <div className="mb-3">
+                                        <input className="form-control p-3" type="password" name="password" id="password" placeholder="Senha:" required value={password} onChange={event =>setPassword(event.target.value)}/>
+                                    </div>
+                                    <button className="btn btn-outline-secondary text-light px-3 fw-bolder" type="submit">Avança</button>
+                                </form>
+                            </div>
                             {/* <div className="card-footer"></div>  */}
                         </div>
                     {/* </div> */}
