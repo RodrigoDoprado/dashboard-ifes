4cc16cc06d85ce0d075d51dfff51cb7fc93967c3
RodrigoDoprado <68282603+RodrigoDoprado@users.noreply.github.com>
2023-10-27 07:53:57 -0300

    fix: page course

--- src/page/Course.tsx
+++ src/page/Course.tsx
@@ -37,7 +37,7 @@ function Course(){
                   course.title?.toLocaleLowerCase().includes(search))
                   .map((item,index) => {
                   return (
-                    <div className="col-sm-4 my-3">
+                    <div className="col-sm-4 mb-3">
                       <div className="card" style={{width: "12rem"}}>
                         <div className="card-body">
                           <h5 className="card-title">{item.title}</h5>
