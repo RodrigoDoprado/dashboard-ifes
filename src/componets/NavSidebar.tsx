ac1c00bdf8f60ca8701d05de2076b805ef0ac304
RodrigoDoprado <68282603+RodrigoDoprado@users.noreply.github.com>
2023-11-10 07:51:38 -0300

    feat: NavSidebar

--- src/componets/NavSidebar.tsx
+++ src/componets/NavSidebar.tsx
@@ -10,14 +10,7 @@ function NavSidebar(){
                         <a className="nav-link" href="/dashboard">
                             <FontAwesomeIcon className='px-2' icon={faHouse} size="lg" />Dashboard
                         </a>
-                        <a className="nav-link collapsed mt-4" href="/alunos" data-bs-toggle="collapse" data-bs-target="#studentLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                            <FontAwesomeIcon className='px-2' icon={faGraduationCap} size="lg" />Alunos
-                        </a>
-                        {/* <div className="collapse" id="studentLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                            <nav className="sb-sidenav-menu-nested nav">
-                                <a className="nav-link" href="/alunos">Home</a>
-                            </nav>
-                        </div> */}
+                        <a className="nav-link collapsed mt-4" href="/alunos"><FontAwesomeIcon className='px-2' icon={faGraduationCap} size="lg" />Alunos</a>
                         <a className="nav-link collapsed mt-4" href="/professores" data-bs-toggle="collapse" data-bs-target="#teacherLayouts" aria-expanded="false" aria-controls="collapseLayouts">
                             <FontAwesomeIcon className='px-2' icon={faChalkboardUser} size="lg" />Professores
                         </a>
