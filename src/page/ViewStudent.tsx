af4c5d55a66cd15851e42e62d5924603a7b3eefb
RodrigoDoprado <68282603+RodrigoDoprado@users.noreply.github.com>
2023-11-17 08:10:22 -0300

    feat: page/viewStudent

--- src/page/ViewStudent.tsx
+++ src/page/ViewStudent.tsx
@@ -18,9 +18,7 @@ function ViewStudent(){
                         <div className="container px-5">
                             <div className="row gx-5 align-items-center">
                                 <div className="col-sm-6">
-                                    <div className="p-5"><img className="img-fluid rounded-circle" src={student?.avatar} alt={student?.firstName+" "+student?.lastName} /></div>
-                                </div>
-                                <div className="col-sm-6">
+                                    <img className="img-fluid rounded-circle" src={student?.avatar} alt={student?.firstName+" "+student?.lastName} />
                                     <div className="p-5">
                                         <h2 className="display-4">{student?.firstName+" "+student?.lastName}</h2>
                                         <p>Matricula: {student?.enroll}</p>
