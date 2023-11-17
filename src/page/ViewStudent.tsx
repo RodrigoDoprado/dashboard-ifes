1f37c3b4b372e7ef6cdc8d45297a12cd17d719d3
RodrigoDoprado <68282603+RodrigoDoprado@users.noreply.github.com>
2023-11-17 07:52:25 -0300

    feat: page/viewStudent

--- src/page/ViewStudent.tsx
+++ src/page/ViewStudent.tsx
@@ -14,19 +14,22 @@ function ViewStudent(){
             <div id="layoutSidenav">
                 <NavSidebarStudent/>
                 <div id="layoutSidenav_content">
-                    <main>
-                        <div className="container-fluid px-4 px-lg-5 mt-5">
-                            <h1>{student?.firstName+" "+student?.lastName}</h1>
-                            <img
-                                className="rounded-circle" 
-                                src={student?.avatar} 
-                                alt={student?.firstName+" "+student?.lastName} 
-                                style={{width:"200px", height:"200px"}}
-                            />
-                            <h1>Matricula: {student?.enroll}</h1>
-                            <h1>Curso: {student?.course?.title}</h1>
+                    <section>
+                        <div class="container px-5">
+                            <div class="row gx-5 align-items-center">
+                                <div class="col-lg-6">
+                                    <div class="p-5"><img class="img-fluid rounded-circle" src={student?.avatar} alt={student?.firstName+" "+student?.lastName} /></div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="p-5">
+                                        <h2 class="display-4">{student?.firstName+" "+student?.lastName}</h2>
+                                        <p>Matricula: {student?.enroll}</p>
+                                        <p>Curso: {student?.course?.title}</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </main>
+                    </section>
                 </div>
             </div>        
         </>
